469b65c908e3843375a692397347965f78e550ca
spk <spk@gmail.com>
2025-03-15 23:08:51 +0530

    portipolio

---
+++ main.js
@@ -0,0 +1,9 @@
+var typed= new Typed(".text",
+    {
+        strings:[ "Web developer", "BussinessMan","Techy"],
+        typeSpeed:100,
+        backSpeed:100,
+        backDelay:1000,
+        loop:true
+
+    });
